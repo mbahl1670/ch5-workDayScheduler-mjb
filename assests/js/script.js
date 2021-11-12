90742e9f46ef6e7c7dc318281d8e9b1d770045aa
Michael <mbahl1670@gmail.com>
2021-11-11 21:07:07 -0600

    function to color code time blocks finished

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,12 +3,66 @@ var textArea9El = document.querySelector("#nine");
 
 
 
+
 var currentHour = moment().format("H");
+    
+var updateHour = function() {
+    $("textarea").addClass("col-9 future");
 
+    if (currentHour == 9) {
+        $("#nine").removeClass().addClass("col-9 present");
+    } else if (currentHour >9) {
+        $("#nine").removeClass().addClass("col-9 past");
+    }
 
+    if (currentHour == 10) {
+        $("#ten").removeClass().addClass("col-9 present");
+    } else if (currentHour >10) {
+        $("#ten").removeClass().addClass("col-9 past");
+    }
 
+    if (currentHour == 11) {
+        $("#eleven").removeClass().addClass("col-9 present");
+    } else if (currentHour >11) {
+        $("#eleven").removeClass().addClass("col-9 past");
+    }
 
+    if (currentHour == 12) {
+        $("#twelve").removeClass().addClass("col-9 present");
+    } else if (currentHour >12) {
+        $("#twelve").removeClass().addClass("col-9 past");
+    }
 
+    if (currentHour == 13) {
+        $("#one").removeClass().addClass("col-9 present");
+    } else if (currentHour >13) {
+        $("#one").removeClass().addClass("col-9 past");
+    }
+
+    if (currentHour == 14) {
+        $("#two").removeClass().addClass("col-9 present");
+    } else if (currentHour >14) {
+        $("#two").removeClass().addClass("col-9 past");
+    }
+
+    if (currentHour == 15) {
+        $("#three").removeClass().addClass("col-9 present");
+    } else if (currentHour >15) {
+        $("#three").removeClass().addClass("col-9 past");
+    }
+
+    if (currentHour == 16) {
+        $("#four").removeClass().addClass("col-9 present");
+    } else if (currentHour >16) {
+        $("#four").removeClass().addClass("col-9 past");
+    }
+
+    if (currentHour == 17) {
+        $("#five").removeClass().addClass("col-9 present");
+    } else if (currentHour >17) {
+        $("#five").removeClass().addClass("col-9 past");
+    }
+}
 
 
 window.onload = function() {
@@ -16,8 +70,6 @@ window.onload = function() {
     var currentTime = moment();
     // Display the current date 
     currentDateEl.textContent = currentTime.format("dddd, MMMM Do");
-    if (currentHour = 22) {
-        textArea9El.className = "col-9 past";
-    }
-
+    console.log(currentHour);
+    updateHour();
 }
